9bc8ab82de7a15ba35fcc1a88f417a90b5c88e25
Joao Victor <jov9051@outlook.com>
2022-04-03 17:27:59 -0300

    feat: funcao handleClient criada em userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,7 @@
+const handleClient = (req, res) => {
+    return res.json({
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
+    });
+};
